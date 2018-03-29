e89a97cf9800249fc13d72c7e84608a7326b11e4
Steve Bachta <sbachta90@gmail.com>
2018-03-28 21:50:59 -0500

    restructure code

--- rest_front/src/main/java/com/rest/front/RestFront.java
+++ rest_front/src/main/java/com/rest/front/RestFront.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FrontApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FrontApplication.class, args);
-	}
+public class RestFront {
+    public static void main(String[] args) {
+        SpringApplication.run(RestFront.class, args); }
 }
