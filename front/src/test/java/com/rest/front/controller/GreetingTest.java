4cfe6385253d58e697d85b04aef9e00af9610dac
Steve Bachta <sbachta90@gmail.com>
2018-04-03 22:01:04 -0500

    remove cached files

--- front/src/test/java/com/rest/front/controller/GreetingTest.java
+++ front/src/test/java/com/rest/front/controller/GreetingTest.java
@@ -1,6 +1,5 @@
 package com.rest.front.controller;
 
-import com.rest.front.FrontApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
